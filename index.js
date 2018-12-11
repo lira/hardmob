1d7dda768d25b5d18bca4a955d83e3ef391d34fb
Fernando Lira <fernando.lira@printi.com.br>
2018-12-11 15:25:24 -0200

    beta version

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const axios = require('axios');
-const url = 'https://www.hardmob.com.br/forums/407-Promocoes?s=&pp=30&daysprune=-1&sort=dateline&order=desc';
+const hardmobUrl = 'https://www.hardmob.com.br/';
+const url = `${hardmobUrl}/forums/407-Promocoes?s=&pp=30&daysprune=-1&sort=dateline&order=desc`;
 const re = /<a class="title" href="(.*)">(.*)<\/a>/g;
+const reGroup =  /href="(.*)" (.*)>(.*)</g;
 
 axios.get(url)
     .then(function (response) {
         const data = response.data;
         const arrayUrls = data.match(re);
-        console.log(arrayUrls);
+	const clean = arrayUrls.map(function (htmlA) {
+	  let data = reGroup.exec(htmlA);
+	  if (data) {
+			  
+	  return {
+	    title: data[3],
+            url: `${hardmobUrl}${data[1]}`,
+	  };
+	  }
+	});
+        console.log(clean);
     })
     .catch(function (error) {
         console.log(error);
