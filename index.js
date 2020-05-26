1ec0c93978fa284c45c4720f6a407cea87164447
Fernando Lira <fernando.lira@printi.com.br>
2020-05-26 20:49:37 -0300

    ajustado caracteres errados iso > utf-8

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const hardmobUrl = 'https://www.hardmob.com.br/';
 const url = `${hardmobUrl}/forums/407-Promocoes?s=&pp=30&daysprune=-1&sort=dateline&order=desc`;
 const re = /<a class="title" href="(.*)">(.*)<\/a>/g;
 
-axios.get(url)
+axios.get(url, { responseType: 'arraybuffer', reponseEncoding: 'binary' })
     .then(function (response) {
-        const data = response.data;
+        const data = response.data.toString('latin1');
         const arrayUrls = data.match(re);
         const clean = arrayUrls.map(function (htmlA) {
             let data = /href="(.*)" (.*)>(.*)</g.exec(htmlA);
@@ -14,7 +14,8 @@ axios.get(url)
                 url: `${hardmobUrl}${data[1]}`,
             };
         });
-        console.log(clean);
+        
+        console.log(JSON.stringify(clean));
     })
     .catch(function (error) {
         console.log(error);
